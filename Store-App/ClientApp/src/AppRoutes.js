3757b1cdfd42b7d10bfe64c9c9f1fa88b7952d9d
joshostblom <josh.ostblom@gmail.com>
2023-10-28 18:02:37 -0500

    Finish folder setup.

--- Store-App/ClientApp/src/AppRoutes.js
+++ Store-App/ClientApp/src/AppRoutes.js
@@ -1,6 +1,6 @@
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
-import { Home } from "./components/Home";
+import { Counter } from "./components/template-examples/Counter/Counter";
+import { FetchData } from "./components/template-examples/FetchData/FetchData";
+import { Home } from "./components/template-examples/Home/Home";
 
 const AppRoutes = [
   {
