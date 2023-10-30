f9deefbe5c174a49ff99e2b2cd844a065b86f11e
Grady Henexson <ghenexson2@huskers.unl.edu>
2023-10-30 17:43:09 -0500

    Added Database connection through SQL Server. Ran with some error on other files that need to be corrected, but otherwise all is good.

---
+++ Store-App/DataDB/DBSQLServerUtils.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Tutorial.SqlConn
+{
+    class DBSQLServerUtils
+    {
+        public static SqlConnection
+                 GetDBConnection(string datasource, string database, string username, string password)
+        {
+            //Data Source=DESKTOP-ET5DFTO\SQLEXPRESS;Initial Catalog=store-app-db;Integrated Security = True
+            
+            string connString = @"Data Source=" + datasource + ";Initial Catalog="
+                        + database + ";Persist Security Info=True;User ID=" + username + ";Password=" + password;
+            SqlConnection conn = new SqlConnection(connString);
+            return conn;
+        }
+    }
+}
---
+++ Store-App/DataDB/DBUtils.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Tutorial.SqlConn
+{
+    class DBUtils
+    {
+        public static SqlConnection GetDBConnection()
+        {
+            string datasource = @"DESKTOP-ET5DFTO\SQLEXPRESS";
+            string database = "store-app-db";
+            string username = "sa";
+            string password = "brand-new-store-2023";
+            return DBSQLServerUtils.GetDBConnection(datasource, database, username, password);
+        }
+    }
+}
