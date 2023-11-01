4b7af0dacc32bb40d4ca18fbc482adb315d93d75
[Ali Keshk] <[akeshk2@huskers.unl.edu]>
2023-11-01 15:12:56 -0500

    created classes

---
+++ Store-App/Models/Classes/Payment.cs
@@ -0,0 +1,18 @@
+﻿namespace Store_App.Models.Classes
+{
+    public class Payment
+    {
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Cvv { get; set; }
+        public DateTime ExpirationDate { get; set; }
+
+        public Payment(string cardName, string cardNumber, string cvv, DateTime expirationDate)
+        {
+            this.CardName = cardName;
+            this.CardNumber = cardNumber;
+            this.Cvv = cvv;
+            this.ExpirationDate = expirationDate;
+        }
+    }
+}
