4fa2ab594d286fba5a05e658441f65b8a4600832
BuildTools <unconfigured@null.spigotmc.org>
2023-11-06 13:16:59 -0600

    Add bootstrap.

--- Store-App/ClientApp/src/pages/LoginPage/LoginPage.js
+++ Store-App/ClientApp/src/pages/LoginPage/LoginPage.js
@@ -1,13 +1,23 @@
-﻿import React, { Component } from 'react';
+﻿import { useState } from "react";
+import { Row, Container } from "react-bootstrap";
 
-export class LoginPage extends Component {
+export const LoginPage = () => {
 
-    render() {
-        return (
-            <div>
-                <h1> Login </h1>
-                <input type="text"/>
-            </div>
-        );
-    }
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <div className="container">
+            <h1> Login </h1>
+            <Row>
+                <input className="text-field" type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+            </Row>
+            <Row>
+                <input className="text-field" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+            </Row>
+            <Row>
+                <button className="btn-primary">Login</button>
+            </Row>
+        </div>
+    );
 }
