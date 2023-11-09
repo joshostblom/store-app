e12ac7d1139b08b47f5b25921a1c57cad83c6ce3
[Ali Keshk] <[akeshk2@huskers.unl.edu]>
2023-11-08 22:50:58 -0600

    added helper class with function to get connection string

---
+++ Store-App/Helpers/ConfigConnectionHelper.cs
@@ -0,0 +1,17 @@
+﻿public static class ConfigHelper
+{
+    private static readonly IConfigurationRoot Configuration;
+
+    static ConfigHelper()
+    {
+        Configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+    }
+
+    public static string GetConnectionString()
+    {
+        return Configuration.GetConnectionString("ShoppingAppCon");
+    }
+}
