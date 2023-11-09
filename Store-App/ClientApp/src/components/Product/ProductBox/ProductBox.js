e167e57525763cec4777fe3d387e021390ffecb9
[Ali Keshk] <[akeshk2@huskers.unl.edu]>
2023-11-08 22:20:25 -0600

    fetch and display products from database

--- Store-App/ClientApp/src/components/Product/ProductBox/ProductBox.js
+++ Store-App/ClientApp/src/components/Product/ProductBox/ProductBox.js
@@ -10,24 +10,24 @@ export const DisplayProductBoxes = () => {
 
     useEffect(() => {
         async function fetchData() {
-            const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+            const response = await fetch('product/getProducts');
             const data = await response.json();
             setProducts(data);
         }
         fetchData();
     }, []);
 
-    //console.log("Products: ", products);
+    console.log("Products: ", products);
 
     return (
         <main>
             {products.length > 0 ? (
                 products.map((product) => (
-                    <div key={product.id} className="product">
+                    <div key={product.productId} className="product">
                         <div className="product-image">
-                            <img src={product.url} alt={product.title} />
+                            <img src={product.imageUrl} alt={product.productName} />
                         </div>
-                        <h5 className="product-title">{product.title}</h5>
+                        <h5 className="product-title">{product.productName}</h5>
                     </div>
                 ))
             ) : (
