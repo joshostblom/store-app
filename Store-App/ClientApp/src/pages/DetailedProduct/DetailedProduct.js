614e0cfa978dc10699195fbee9539e442bbde5f4
[Ali Keshk] <[akeshk2@huskers.unl.edu]>
2023-11-18 14:39:51 -0600

    format information

--- Store-App/ClientApp/src/pages/DetailedProduct/DetailedProduct.js
+++ Store-App/ClientApp/src/pages/DetailedProduct/DetailedProduct.js
@@ -17,6 +17,10 @@ export const DisplayDetailedProduct = () => {
             });
     }, [productId]);
 
+    const description = "Description: " + productById?.descript;
+    const manufacturer = "Manufacturer: " + productById?.manufacturerInformation;
+    const dimensions = "Dimensions: \"" + productById?.prodHeight + "\" X \"" + productById?.prodWidth + "\" X \"" + productById?.prodLength + "\" X \"" + productById?.prodWeight + "\"";
+
     return (
         <div className="outer-container" style={{ paddingTop: '200px' }}>
             <table style={{ width: '100%', marginTop: '10px', borderCollapse: 'collapse' }}>
@@ -27,10 +31,13 @@ export const DisplayDetailedProduct = () => {
                         </div>
                     </Row>
                     <Row className="justify-content-md-center">
-                        <h6 className="product-text">{productById?.descript}</h6>
+                        <h6 className="product-text">{description}</h6>
                     </Row>
                     <Row className="justify-content-md-center">
-                        <h6 className="product-text">{productById?.manufacturerInformation}</h6>
+                        <h6 className="product-text">{manufacturer}</h6>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                        <h6 className="product-text">{dimensions}</h6>
                     </Row>
                     <Row className="justify-content-md-center">
                         <div style={{ display: 'flex', justifyContent: 'right' }}>
